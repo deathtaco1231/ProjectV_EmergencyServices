20710912835b21bf8b7f4296f893d408cece9734
Julian Cumming <jcumming@conestogac.on.ca>
2024-10-25 23:06:51 -0400

    - Added basic supabase code - Basic config of project complete - Sample code also added and further work on table schema and other areas to come soom

---
+++ EmergencyServices/ForumPost.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Postgrest.Models;
+using Postgrest.Attributes;
+using Supabase.Core.Attributes;
+
+namespace EmergencyServices.Group8
+{
+    [Table("forum_posts")]
+    public class ForumPost : BaseModel
+    {
+        [PrimaryKey("id")]
+        public int Id { get; set; }
+
+        [Column("created_at")]
+        public DateTime createdAt { get; set; }
+
+        [Column("user_name")]
+        public string userName { get; set; }
+
+        [Column("post_header")]
+        public string postHeader { get; set; }
+
+        [Column("post_body")]
+        public string postBody { get; set; }
+
+        public override string ToString()
+        {
+            return Id.ToString() + "," + createdAt.ToString() + "," + userName + "," + postHeader + "," + postBody;
+        }
+    }
+}
--- EmergencyServices/Program.cs
+++ EmergencyServices/Program.cs
@@ -1,15 +1,52 @@
-﻿using System;
+﻿//#define TESTING
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace EmergencyServices
+namespace EmergencyServices.Group8 // Likely will just be used for testing since 
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
+            DotNetEnv.Env.Load();
+
+            var url = Environment.GetEnvironmentVariable("SUPABASE_URL");
+            var key = Environment.GetEnvironmentVariable("SUPABASE_KEY");
+            var options = new Supabase.SupabaseOptions
+            {
+                AutoConnectRealtime = true
+            };
+
+            var supabase = new Supabase.Client(url, key, options);
+            await supabase.InitializeAsync();
+
+            var result = await supabase.From<ForumPost>().Get();
+            var cities = result.Models;
+
+            if (cities.Count == 0) // this for testing
+                Console.WriteLine("No rows.");
+            else // this also for testing
+            {
+                foreach (var c in cities)
+                {
+                    Console.WriteLine(c.ToString());
+                }
+            }
+#if TESTING
+            var model = new ForumPost
+            {
+                userName = "Test Insert User Name",
+                postHeader = "Test Insert Post Header",
+                postBody = "Test Insert Post Body",
+                createdAt = DateTime.Now
+            };
+            await supabase.From<ForumPost>().Insert(model);
+            #endif
+            Console.Read();
         }
     }
 }
